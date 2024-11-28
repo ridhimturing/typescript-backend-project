14e04eaf9b1f9355f78c9d9ba54b1e29f2436423
ridhi <ridhi.mathur@creativevirtual.com>
2024-11-28 13:56:22 +0530

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+// src/index.ts
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('Hello, TypeScript with Travis CI!');
+});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// src/index.ts
+
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript with Travis CI!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
